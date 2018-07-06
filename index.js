a1350d9be764077b927871f118d2d566fa88330b
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 03:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+
+function inc(n){
+  return n++;
+}
+
+function dec(n){
+  return n--;
+}
+
 function makeInt(n){
-  return parseInt(n);
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n){
